c1342e6e33759563ba9dbec1af261539baea5d9d
Kevin Day <kday@tamu.edu>
2025-10-13 15:28:46 -0500

    MODCAMUNDA-48: Resolve excessive logging due to polyglot warnings.
    The use of spring-boot to perform auto-configuration makes this process a little different than the suggested solution from the warning log.
    
    Implement a Camunda configuration file, incrementing from the default order to ensure that it happens after the camunda configurations are loaded.
    Then implement a new custom engine.
    
    The new custom engine, also defined with an incremented from default order, sets a custom scripting engine resolver.
    
    This custom scripting engine resolver overrides only the JavaScript engine.
    The `engine.WarnInterpreterOnly` is then explicitly set to `false`.
    The `engine.allowExperimentalOptions(true)` is set based on the documentation referenced below.
    
    The `allowHostClassLookup()` defaults to `true`, but this is being explicitly set to `true` just in case Camunda suddenly changes their defaults.
    The design of mod-workflow and mod-camunda specifically rely on the host class lookup behavior.
    
    see: https://docs.camunda.org/manual/latest/user-guide/process-engine/scripting/#custom-scriptengineresolver
    see: https://docs.camunda.org/manual/latest/user-guide/spring-boot-integration/configuration/#camunda-engine-properties
    see: https://docs.camunda.org/manual/latest/reference/deployment-descriptors/tags/process-engine/#configuration-properties
    see: https://forum.camunda.io/t/console-filling-with-graalvm-warnings-when-using-in-process-scripting/31812

---
+++ src/main/java/org/folio/rest/camunda/resolver/ScriptEngineResolver.java
@@ -0,0 +1,37 @@
+package org.folio.rest.camunda.resolver;
+
+import com.oracle.truffle.js.scriptengine.GraalJSScriptEngine;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import org.camunda.bpm.engine.impl.scripting.engine.DefaultScriptEngineResolver;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Engine;
+
+/**
+ * A custom script engine resolver to allow for setting configuration.
+ *
+ * This can be used to configure any supported scripting engine.
+ *
+ * Note: To enable Nashorn compatibility, enable the builders experimental options  allowExperimentalOptions(true).
+ *       Then set .option("js.nashorn-compat", "true").
+ */
+public class ScriptEngineResolver extends DefaultScriptEngineResolver {
+
+    public ScriptEngineResolver(ScriptEngineManager scriptEngineManager) {
+        super(scriptEngineManager);
+    }
+
+    @Override
+    protected ScriptEngine getJavaScriptScriptEngine(String language) {
+        Engine engine = Engine.newBuilder()
+            .allowExperimentalOptions(true)
+            .option("engine.WarnInterpreterOnly", "false")
+            .build();
+
+        Context.Builder builder = Context.newBuilder("js")
+          // Make sure GraalVM JS can provide access to the host and can lookup classes.
+          .allowHostClassLookup(s -> true);
+
+        return GraalJSScriptEngine.create(engine, builder);
+    }
+}
